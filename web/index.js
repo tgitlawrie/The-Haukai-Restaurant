f3302052dcf07717ba659060242f0d171ef91864
T <tlawrie@orcon.net.nz>
2022-09-05 10:15:19 +1200

    initial commit

---
+++ web/index.js
@@ -0,0 +1,26 @@
+function myFunction() {
+  var x = document.getElementById("navbar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
+
+// slideshow for index.html
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 4000); // Change image every 4 seconds
+}
